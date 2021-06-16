a53137320bdbf8b164528a0fdb212cb5bcbf3518
Tomohito Inoue <hypernumbernet@users.noreply.github.com>
2021-06-16 14:35:05 +0900

    setDepthBufferSize 32 to 24 because my note pc does not work

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
     format.setRenderableType(QSurfaceFormat::OpenGL);
     format.setVersion(3, 3);
     format.setProfile(QSurfaceFormat::CoreProfile);
-    format.setDepthBufferSize(32);
+    format.setDepthBufferSize(24);
     QSurfaceFormat::setDefaultFormat(format);
 
     MainWidget widget;
