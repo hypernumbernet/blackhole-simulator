531aad39e800c415aa052e69faa69bdccacc8f50
Tomohito Inoue <hypernumbernet@users.noreply.github.com>
2021-07-09 21:51:52 +0900

    Implemented GPU 3D-Coordinate 3S-Velocity double calculation.

--- computeshaders.cpp
+++ computeshaders.cpp
@@ -1,94 +1,5 @@
 #include "computeshaders.h"
 
-#include <QDebug>
-#include <iterator>
-
-#ifdef QT_DEBUG
-void APIENTRY gl_debug_callback(
-        GLenum source, GLenum type, GLuint id,
-        GLenum severity, GLsizei/* length*/, const GLchar* msg, const void* /*data*/)
-{
-    QString source_str;
-    QString type_str;
-    QString severity_str;
-
-    switch (source)
-    {
-    case GL_DEBUG_SOURCE_API:
-        source_str = "SOURCE_API";
-        break;
-    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-        source_str = "SOURCE_WINDOW_SYSTEM";
-        break;
-    case GL_DEBUG_SOURCE_SHADER_COMPILER:
-        source_str = "SOURCE_SHADER_COMPILER";
-        break;
-    case GL_DEBUG_SOURCE_THIRD_PARTY:
-        source_str = "SOURCE_THIRD_PARTY";
-        break;
-    case GL_DEBUG_SOURCE_APPLICATION:
-        source_str = "SOURCE_APPLICATION";
-        break;
-    case GL_DEBUG_SOURCE_OTHER:
-        source_str = "SOURCE_OTHER";
-        break;
-    default:
-        source_str = "?";
-        break;
-    }
-
-    switch (type)
-    {
-    case GL_DEBUG_TYPE_ERROR:
-        type_str = "TYPE_ERROR";
-        break;
-    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-        type_str = "TYPE_DEPRECATED_BEHAVIOR";
-        break;
-    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-        type_str = "TYPE_UNDEFINED_BEHAVIOR";
-        break;
-    case GL_DEBUG_TYPE_PORTABILITY:
-        type_str = "TYPE_PORTABILITY";
-        break;
-    case GL_DEBUG_TYPE_PERFORMANCE:
-        type_str = "TYPE_PERFORMANCE";
-        break;
-    case GL_DEBUG_TYPE_OTHER:
-        type_str = "TYPE_OTHER";
-        break;
-    case GL_DEBUG_TYPE_MARKER:
-        type_str = "TYPE_MARKER";
-        break;
-    default:
-        type_str = "?";
-        break;
-    }
-
-    switch (severity)
-    {
-    case GL_DEBUG_SEVERITY_HIGH:
-        severity_str = "SEVERITY_HIGH";
-        break;
-    case GL_DEBUG_SEVERITY_MEDIUM:
-        severity_str = "SEVERITY_MEDIUM";
-        break;
-    case GL_DEBUG_SEVERITY_LOW:
-        severity_str = "SEVERITY_LOW";
-        break;
-    case GL_DEBUG_SEVERITY_NOTIFICATION:
-        severity_str = "SEVERITY_NOTIFICATION";
-        break;
-    default:
-        severity_str = "?";
-        break;
-    }
-
-    if (source == GL_DEBUG_SOURCE_APPLICATION)
-        qDebug() << "[" << source_str << "][" << type_str << "][" << id << "][" << severity_str << "] " << msg;
-}
-#endif
-
 ComputeShaders::ComputeShaders()
 {
 }
@@ -137,13 +48,6 @@ bool ComputeShaders::initialize()
     if (!addShader(&m_programInteraction3D4DDouble, ":/shader/interaction3D4D_d.comp"))
         return false;
 
-#ifdef QT_DEBUG
-    glEnable(GL_DEBUG_OUTPUT);
-    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-    glDebugMessageCallback(gl_debug_callback, nullptr);
-    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
-#endif
-
     return true;
 }
 
@@ -171,6 +75,7 @@ void ComputeShaders::bind(const AbstractNBodyEngine<double>* const engine)
     case bhs::Engine::G3DEuler:
         m_programTimeProgressUsing = &m_programTimeProgressDouble;
         m_programInteractionUsing = &m_programInteractionDouble;
+        break;
     case bhs::Engine::G3D4DEuler:
         m_programTimeProgressUsing = &m_programTimeProgress3D4DDouble;
         m_programInteractionUsing = &m_programInteraction3D4DDouble;
