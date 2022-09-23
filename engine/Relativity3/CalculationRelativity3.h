a40be5c6902d2c66f7bd2eeb30f05194c4a3c85f
Tomohito Inoue <hypernumbernet@users.noreply.github.com>
2022-09-23 16:28:07 +0900

    update CalculationRelativity3

--- engine/Relativity3/CalculationRelativity3.h
+++ engine/Relativity3/CalculationRelativity3.h
@@ -14,8 +14,8 @@ public:
         , m_timeProgresEnd(engine->timeProgressRanges().at(threadNumber).end)
         , m_interactionStart(engine->interactionRanges().at(threadNumber).start)
         , m_interactionEnd(engine->interactionRanges().at(threadNumber).end)
-        , m_velocityPerAngle((SPEED_OF_LIGHT * engine->scaleInv()) / PI)
-        , m_anglePerVelocity(PI / (SPEED_OF_LIGHT * engine->scaleInv()))
+        , m_ct(SPEED_OF_LIGHT * engine->scaleInv() * engine->timePerFrame())
+        , m_speedOfLightInv(1.0 / (SPEED_OF_LIGHT * engine->scaleInv()))
     {
     }
 
@@ -23,22 +23,22 @@ public:
     {
         double* const coordinates = m_engine->coordinates();
         double* const velocities = m_engine->velocities();
-        const double timePerFrame = m_engine->timePerFrame();
 
-        Quaternion vq;
-        Vector3 v3;
-        quint64 i3;
+        quint64 i4, i3;
 
         for (quint64 i = m_timeProgresStart; i < m_timeProgresEnd; ++i)
         {
-            vq.set(velocities, i * 4);
-            v3.set(vq.lnV3());
-            v3 *= m_velocityPerAngle;
-            v3 *= timePerFrame;
+            i4 = i * 4;
             i3 = i * 3;
-            coordinates[i3] += v3.x(); ++i3;
-            coordinates[i3] += v3.y(); ++i3;
-            coordinates[i3] += v3.z();
+            double tau = m_ct / velocities[i4];
+            if ( ! std::isfinite(tau))
+            {
+                //qDebug() << "tau: " << tau << i;
+                continue;
+            }
+            coordinates[i3    ] += velocities[i4 + 1] * tau;
+            coordinates[i3 + 1] += velocities[i4 + 2] * tau;
+            coordinates[i3 + 2] += velocities[i4 + 3] * tau;
         }
     }
 
@@ -55,29 +55,31 @@ public:
         const double boundaryToInvalidate = AbstractNBodyEngine<double>::BOUNDARY_TO_INVALIDATE;
 
         quint64 i, j, a, b, i4;
+        Vector3 acceleration, acc;
         Quaternion qd;
         double d1, d2, d3, r, inv, theta;
+        double accAbs;
+        double accBetaAngle;
+        double sinBeta;
 
         static const int w = 1;
         static const int x = 2;
         static const int y = 4;
         static const int z = 6;
         Octonion origin(0.);
+        origin[w] = 1.;
         Octonion ov;
         Octonion od;
         Octonion pole;
         Octonion rotation;
-        origin[w] = 1.;
 
         for (i = m_timeProgresStart; i < m_timeProgresEnd; ++i)
         {
             a = i * 3;
             i4 = i * 4;
-            ov.set(0.);
-            ov[w] = velocities[i4    ];
-            ov[x] = velocities[i4 + 1];
-            ov[y] = velocities[i4 + 2];
-            ov[z] = velocities[i4 + 3];
+
+            acceleration.set(0., 0., 0.);
+
             for (j = 0; j < numberOfParticles; ++j)
             {
                 if (i == j)
@@ -90,22 +92,39 @@ public:
                 if (r <= boundaryToInvalidate)
                     continue;
                 inv = 1.0 / r;
-                d1 *= inv;
-                d2 *= inv;
-                d3 *= inv;
-                theta = inv * inv * timeG * masses[j];
-                theta *= m_anglePerVelocity;
-
-                qd.set(Quaternion::rotation(d1, d2, d3, 1.));
-                od.set(0.);
-                od[w] = qd.re();
-                od[x] = qd.i1();
-                od[y] = qd.i2();
-                od[z] = qd.i3();
-                pole.set(od.cross(origin));
-                rotation.set(Octonion::rotation(pole, theta));
-                ov.set(rotation.conjugated() * ov * rotation);
+                theta = inv * inv * inv * timeG * masses[j];
+                acc.set(d1 * theta, d2 * theta, d3 * theta);
+                acceleration += acc;
             }
+
+            accAbs = acceleration.abs();
+            if (accAbs == 0.)
+                continue;
+            acceleration /= accAbs;
+            accBetaAngle = 0.5 * PI * accAbs * m_speedOfLightInv;
+            sinBeta = sin(accBetaAngle);
+            qd.setRe(cos(accBetaAngle));
+            qd.setI1(sinBeta * acceleration.x());
+            qd.setI2(sinBeta * acceleration.y());
+            qd.setI3(sinBeta * acceleration.z());
+
+
+            ov.set(0.);
+            ov[w] = velocities[i4    ];
+            ov[x] = velocities[i4 + 1];
+            ov[y] = velocities[i4 + 2];
+            ov[z] = velocities[i4 + 3];
+
+            qd.set(Quaternion::rotation(acceleration, 1.));
+            od.set(0.);
+            od[w] = qd.re();
+            od[x] = qd.i1();
+            od[y] = qd.i2();
+            od[z] = qd.i3();
+            pole.set(od.cross(origin));
+            rotation.set(Octonion::rotation(pole, accBetaAngle));
+            ov.set(rotation.conjugated() * ov * rotation);
+
             velocities[i4    ] = ov[w];
             velocities[i4 + 1] = ov[x];
             velocities[i4 + 2] = ov[y];
@@ -121,6 +140,6 @@ private:
     const quint64 m_interactionStart;
     const quint64 m_interactionEnd;
 
-    const double m_velocityPerAngle;
-    const double m_anglePerVelocity;
+    const double m_ct;
+    const double m_speedOfLightInv;
 };
--- engine/Relativity3/InitializerRelativity3.h
+++ engine/Relativity3/InitializerRelativity3.h
@@ -24,27 +24,30 @@ private:
     {
         const quint64 num = m_engine->numberOfParticle();
         T* const velocities = m_engine->velocities();
+
         T* vels = new T[num * 4]();
-        const double anglePerVelocity = PI / (SPEED_OF_LIGHT * m_engine->scaleInv());
+
+        const double speedOfLight = SPEED_OF_LIGHT * m_engine->scaleInv();
+        const double ct = speedOfLight * m_engine->timePerFrame();
+        const double speedOfLightInv = 1. / speedOfLight;
 
         for (quint64 i = 0; i < num; ++i)
         {
             const quint64 i3 = i * 3;
             const quint64 i4 = i * 4;
-            Vector3 v3((double)velocities[i3], (double)velocities[i3 + 1], (double)velocities[i3 + 2]);
-            const double speed = v3.abs();
-            if (speed != 0.)
-            {
-                v3 /= speed;
-            }
-            const auto angle = speed * anglePerVelocity;
-            auto q = Quaternion::exp(v3 * angle);
-            bhs::embedQuaternionToArray<T>(q, vels, i4);
+            const Vector3 v((double)velocities[i3], (double)velocities[i3 + 1], (double)velocities[i3 + 2]);
+            Spacetime st(ct);
+            st.lorentzTransformation(v, speedOfLightInv);
+            vels[i4    ] = st.w();
+            vels[i4 + 1] = st.x();
+            vels[i4 + 2] = st.y();
+            vels[i4 + 3] = st.z();
         }
         for (quint64 i = 0; i < num * 4; ++i)
         {
             velocities[i] = vels[i];
         }
+
         delete[] vels;
     }
 
